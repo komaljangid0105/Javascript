b679c24b97c8d5291693c89b6294bdaf9284ff29
komaljangid0105 <157354542+komaljangid0105@users.noreply.github.com>
2024-02-16 11:00:48 +0000

    conversion of datatype

---
+++ 01_basics/conversion.js
@@ -0,0 +1,24 @@
+let score = "33ab"
+ 
+console.log(typeof score)
+console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log( valueInNumber)
+
+//33 => converted 33 
+//komal = > NaN 
+//true => conveerted to 1
+//komal4 = >NaN
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+console.log( typeof booleanIsLoggedIn)
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
